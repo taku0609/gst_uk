959f5d9233f055c8a05760e76dcf5f446d44b0f7
tak0609 <78605227+taku0609@users.noreply.github.com>
2023-10-02 09:33:06 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: "#da373d",
+            },
+            keyframes: {
+                'fade-in-down': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(0px)'
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)'
+                    },
+                }
+            },
+            animation: {
+                'fade-in-down': 'fade-in-down 2.5s ease-out'
+            }
+        },
+    },
+}
